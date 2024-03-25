db3dcd1dfb921af4a6a5548430228a9eec85da03
Justin <justinlim60@gmail.com>
2024-03-26 00:34:01 +0800

    fix able to make meeting in the past bug

--- src/main/java/seedu/address/logic/parser/ScheduleMeetingCommandParser.java
+++ src/main/java/seedu/address/logic/parser/ScheduleMeetingCommandParser.java
@@ -7,11 +7,12 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_MEETING_DATE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_MEETING_DURATION;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_MEETING_NOTES;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_MEETING_TIME;
+import static seedu.address.logic.parser.ParserUtil.parseLocalDateTime;
 
 import java.time.Duration;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
-import java.time.format.DateTimeParseException;
 
 import seedu.address.commons.core.index.Index;
 import seedu.address.commons.exceptions.IllegalValueException;
@@ -54,19 +55,25 @@ public class ScheduleMeetingCommandParser implements Parser<ScheduleMeetingComma
         if (!argMultimap.getValue(PREFIX_MEETING_AGENDA).isPresent()) {
             throw new ParseException(Meeting.MESSAGE_CONSTRAINTS_AGENDA);
         }
-
-        LocalDate meetingDate;
-        LocalTime meetingTime;
+        LocalDateTime meetingDateTime;
         Duration duration;
         try {
-            meetingDate = ParserUtil.parseDate(argMultimap.getValue(PREFIX_MEETING_DATE).orElse(""));
-            meetingTime = ParserUtil.parseTime(argMultimap.getValue(PREFIX_MEETING_TIME).orElse(""));
+            String stringMeetingDate = argMultimap.getValue(PREFIX_MEETING_DATE).orElse("");
+            String stringMeetingTime = argMultimap.getValue(PREFIX_MEETING_TIME).orElse("");
+            meetingDateTime = parseLocalDateTime(stringMeetingDate, stringMeetingTime);
             duration = ParserUtil.parseDuration(argMultimap.getValue(PREFIX_MEETING_DURATION).orElse(""));
-        } catch (DateTimeParseException dtpe) {
+        } catch (ParseException e) {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
-                    ScheduleMeetingCommand.MESSAGE_USAGE), dtpe);
+                    ScheduleMeetingCommand.MESSAGE_USAGE), e);
         }
-
+        LocalDate today = LocalDate.now();
+        LocalDate meetingDate = meetingDateTime.toLocalDate();
+        LocalTime meetingTime = meetingDateTime.toLocalTime();
+        if (meetingDate.isAfter(today.plusYears(1))) { // Assuming 1 year is too far in the future
+            throw new ParseException("Cannot schedule a meeting more than a year in the future.");
+        } else if (meetingDate.isBefore(today)) {
+            throw new ParseException("Cannot schedule a meeting in the past.");
+        }
         String agenda = argMultimap.getValue(PREFIX_MEETING_AGENDA)
                 .orElseThrow(() -> new ParseException("Agenda is required."));
 
