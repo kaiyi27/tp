b9ec9ac7c8839c40be232b857afa6d25c1c787a2
Ibnu Taimiyyah <ibnu2651@gmail.com>
2024-04-02 22:52:11 +0800

    Add tests for client status summary values

--- src/main/java/seedu/address/model/person/ClientStatusSummaryValues.java
+++ src/main/java/seedu/address/model/person/ClientStatusSummaryValues.java
@@ -1,9 +1,13 @@
 package seedu.address.model.person;
 
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
 /**
  * Represents the summary values of clients' status in the person list.
  */
 public class ClientStatusSummaryValues {
+    public static final String MESSAGE_CONSTRAINTS =
+            "Number of clients and sum of number of client statuses are unequal";
     public final int numberOfClients;
     public final int numberOfStart;
     public final int numberOfMid;
@@ -13,9 +17,28 @@ public class ClientStatusSummaryValues {
      * Creates a {@code ClientStatusSummaryValues} with the given values.
      */
     public ClientStatusSummaryValues(int numClients, int numStart, int numMid, int numEnd) {
+        checkArgument(numClients == numStart + numMid + numEnd);
         numberOfClients = numClients;
         numberOfStart = numStart;
         numberOfMid = numMid;
         numberOfEnd = numEnd;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof ClientStatusSummaryValues)) {
+            return false;
+        }
+
+        ClientStatusSummaryValues otherClientStatusSummaryValues = (ClientStatusSummaryValues) other;
+        return this.numberOfClients == otherClientStatusSummaryValues.numberOfClients
+                && this.numberOfStart == otherClientStatusSummaryValues.numberOfStart
+                && this.numberOfMid == otherClientStatusSummaryValues.numberOfMid
+                && this.numberOfEnd == otherClientStatusSummaryValues.numberOfEnd;
     }
 }
